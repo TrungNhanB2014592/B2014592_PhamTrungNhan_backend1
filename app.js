a606f070f8212d86efafa2f7b266ee4ed0d27966
Nhanb2014592 <Nhanb2014592@student.ctu.edu.vn>
2023-10-04 16:12:52 +0700

    commit

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
 const contactRoutes = require("./app/routes/contact.route");
 const ApiError = require("./app/api-error");
+const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/contacts", contactRoutes);
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
 });
-app.use("/api/contacts", contactRoutes);
+
 app.use((req, res, next) => {
   return next(new ApiError(404, "Resource not found."));
 });
@@ -18,5 +20,4 @@ app.use((err, req, res, next) => {
     message: err.message || "Internal Server Error",
   });
 });
-
 module.exports = app;
